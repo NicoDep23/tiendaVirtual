132e3835513c9da92ed6fc9dc52e61f80a1be813
NicoDep23 <nicolasdepasquale16@gmail.com>
2024-05-23 22:06:02 -0300

    home y proudctos + rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo:'/home', pathMatch:'full'},
+    {path:'home', component: HomeComponent},
+    {path:'productos', component: ProductosComponent}
+
+];
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+  @Input() title: string | undefined;
+  @Input() imageUrl: string | undefined;
+  @Input() description: string | undefined;
+  @Input() price: string | undefined;
 }
